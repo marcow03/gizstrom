c181cd702f2273f2e3f7646cc6ed5f583fe37588
marcow03 <macwei26@gmail.com>
2026-05-07 21:24:59 +0200

    fix: add image build script

--- build_images_debug.sh
+++ build_images_debug.sh
@@ -2,3 +2,5 @@
 
 docker build -t ghcr.io/marcow03/gizstrom/app:latest ./src/app/
 docker build -t ghcr.io/marcow03/gizstrom/pipelines:latest ./src/pipelines/
+docker build -t ghcr.io/marcow03/gizstrom/airflow:latest ./infra/airflow/
+docker build -t ghcr.io/marcow03/gizstrom/feast-ui:latest ./infra/feast-ui/
